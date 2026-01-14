02e71472e22665f67efb6633c8fba2a5dc38e733
Alwan <alwanfdhlrhmn@gmail.com>
2026-01-14 12:36:53 +0700

    Change Code for Final Practice

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@
 #define LED_MERAH D1
 
 // WIFI CONFIG
-const char *ssid = "isi nama wifi sendiri";
-const char *password = "isi password wifi sendiri";
+const char *ssid = "awann";
+const char *password = "hayoloapa";
 
 // MQTT CONFIG
-const char *mqtt_servcer = "154.19.37.27";
+const char *mqtt_server = "154.19.37.27";
 const int mqtt_port = 1883;
 const char *mqtt_user = "yudhi";
 const char *mqtt_pass = "yudhi123";
@@ -19,6 +19,10 @@ const char *mqtt_topic = "esp8266/led";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+String clientId;
+
+void mqttCallback(char *topic, byte *payload, unsigned int length);
 
 void setupWiFi()
 {
@@ -41,21 +45,74 @@ void setup()
 
   setupWiFi();
 
+  clientId = "D1Mini-" + String(ESP.getChipId());
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(mqttCallback);
+
   // Matikan semua LED saat awal
   digitalWrite(LED_HIJAU, LOW);
   digitalWrite(LED_MERAH, LOW);
 }
 
-void loop()
+void mqttCallback(char *topic, byte *payload, unsigned int length)
 {
-  // LED Hijau ON, Merah OFF
-  digitalWrite(LED_HIJAU, HIGH);
-  digitalWrite(LED_MERAH, LOW);
-  delay(1000);
+  String message;
 
-  // LED Hijau OFF, Merah ON
-  digitalWrite(LED_HIJAU, LOW);
-  digitalWrite(LED_MERAH, HIGH);
-  delay(1000);
+  for (unsigned int i = 0; i < length; i++)
+  {
+    message += (char)payload[i];
+  }
+
+  message.trim();
+
+  Serial.println("MQTT x [" + String(topic) + "] => " + message);
+
+  if (message == "ON Hijau")
+  {
+    Serial.println("Hijau Masuk");
+    digitalWrite(LED_HIJAU, HIGH);
+    digitalWrite(LED_MERAH, LOW);
+  }
+  else if (message == "ON Merah")
+  {
+    digitalWrite(LED_HIJAU, LOW);
+    digitalWrite(LED_MERAH, HIGH);
+  }
 }
 
+void reconnectMQTT()
+{
+  while (!client.connected())
+  {
+    Serial.print("Connecting MQTT... ");
+
+    if (client.connect(
+            clientId.c_str(),
+            mqtt_user,
+            mqtt_pass))
+    {
+
+      Serial.println("CONNECTED");
+      client.subscribe(mqtt_topic);
+
+      // Kirim status ke cloud
+      client.publish("esp8266/status", "ONLINE");
+    }
+    else
+    {
+      Serial.print("FAILED, rc=");
+      Serial.print(client.state());
+      delay(3000);
+    }
+  }
+}
+
+void loop()
+{
+   if (!client.connected()) {
+    reconnectMQTT();
+  }
+
+  client.loop();
+}
